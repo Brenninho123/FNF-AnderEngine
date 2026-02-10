8a8510d98ad568212759235706fe607077e0a024
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-02-10 14:26:34 -0300

    Refactor mobile touch pad handling
    Removed conditional touch pad visibility for non-Android mobile.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1103,11 +1103,8 @@ class PlayState extends MusicBeatState
 		// startCountdown();
 
 		#if mobile
-		#if !android
 		addTouchPad("NONE", "P");
 		addTouchPadCamera();
-		touchPad.visible = true;
-		#end
 		#end
 		
 		#if mobile
